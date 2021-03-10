51a0d03286438e93a99d3dd32319a6de21fc1027
Sayum24 <samuel.the.hashtag@gmail.com>
2021-03-10 16:02:45 +0100

    start - fonts - cruiser - astroids array - close event

--- ct_ka_game.py
+++ ct_ka_game.py
@@ -3,3 +3,32 @@ import sys
 import random
 
 
+pygame.init()
+
+screen = pygame.display.set_mode([800, 400])
+
+clock = pygame.time.Clock()
+
+score_font = pygame.font.SysFont("Calibri", 30, True, False)
+gameover_font = pygame.font.SysFont("Calibri", 69, True, False)
+
+text_gamover = gameover_font.render("Game Over!", True, (255, 5, 0))
+
+space_cruiser = [400]
+
+score = 0
+end = False
+
+astroids = []
+
+
+
+
+
+while True:
+
+    # Events
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT: # close-button
+            sys.exit()
